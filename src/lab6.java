2170c04663d1816681fb76aef756ffeb8ef2b1e1
ChristopherBarroso <cbarroso1@ufl.edu>
2022-03-12 18:05:40 -0500

    first commit

---
+++ src/lab6.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class lab6 {
+    public static void main (String[] args){
+
+        int number1 = 0;
+        int number2 = 0;
+        int result = 0;
+
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.print("Number 1: ");
+        number1 = scnr.nextInt();
+
+        System.out.print("\nNumber 2: ");
+        number2 = scnr.nextInt();
+
+        result = number1 + number2;
+        System.out.println("\nThe sum is: " + result);
+    }
+
+}
